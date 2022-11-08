0fb7e972d19fd5ffd7bd21faf08546b862790aa8
zeinaranda <zeinaranda66@gmail.com>
2022-11-09 03:12:44 +0700

    header home

--- app/src/main/java/com/capstone/curhatin/MainActivity.kt
+++ app/src/main/java/com/capstone/curhatin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.capstone.curhatin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.capstone.curhatin.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
