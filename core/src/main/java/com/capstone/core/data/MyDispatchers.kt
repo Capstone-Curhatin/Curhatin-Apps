9c70f67a5fa9ea4c2b2ad1af59279ae9237e9d30
wahidabd <wahed.blog99@gmail.com>
2022-11-09 02:05:31 +0700

    feat: clean architecture in login implementation

---
+++ core/src/main/java/com/capstone/core/data/MyDispatchers.kt
@@ -0,0 +1,10 @@
+package com.capstone.core.data
+
+import kotlinx.coroutines.Dispatchers
+
+class MyDispatchers {
+    val default = Dispatchers.Default
+    val io = Dispatchers.IO
+    val main = Dispatchers.Main
+    val unconfined = Dispatchers.Unconfined
+}
