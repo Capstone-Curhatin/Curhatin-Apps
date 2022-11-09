e3f70c5442d1fe4978dc81a11e7bf39b07e19812
YudhistiraKusuma202310067 <202310067@ibik.ac.id>
2022-11-09 13:40:41 +0700

    login screen

--- app/src/main/java/com/capstone/curhatin/MainActivity.kt
+++ app/src/main/java/com/capstone/curhatin/MainActivity.kt
@@ -2,17 +2,10 @@ package com.capstone.curhatin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.capstone.curhatin.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: FragmentHomeBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
-
 }
