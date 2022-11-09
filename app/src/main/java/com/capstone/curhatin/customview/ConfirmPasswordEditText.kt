a9b98bcfc12dca23c9d0636d7273e223ff9e7898
YudhistiraKusuma202310067 <202310067@ibik.ac.id>
2022-11-09 23:39:51 +0700

    forgot and new password screen

---
+++ app/src/main/java/com/capstone/curhatin/customview/ConfirmPasswordEditText.kt
@@ -0,0 +1,72 @@
+package com.capstone.curhatin.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.capstone.curhatin.R
+
+class ConfirmPasswordEditText : AppCompatEditText {
+
+    private lateinit var confirmPasswordIconDrawable: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        transformationMethod = PasswordTransformationMethod.getInstance()
+    }
+
+    private fun init() {
+        confirmPasswordIconDrawable =
+            ContextCompat.getDrawable(context, R.drawable.icons8_lock_2) as Drawable
+        inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+        compoundDrawablePadding = 16
+
+        setHint(R.string.confirm_password)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setAutofillHints(AUTOFILL_HINT_PASSWORD)
+        }
+        setDrawable(confirmPasswordIconDrawable)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty())
+                    error = context.getString(R.string.et_password_null_error_message)
+            }
+        })
+    }
+
+    private fun setDrawable(
+        start: Drawable? = null,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
+    }
+}
