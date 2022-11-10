6337c8598cd95c3b02ce9cd56c92a492244857be
wahidabd <wahed.blog99@gmail.com>
2022-11-10 12:56:19 +0700

    fix conflict in core module

---
+++ app/src/main/java/com/capstone/curhatin/customview/StrEditText.kt
@@ -0,0 +1,65 @@
+package com.capstone.curhatin.customview
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.capstone.curhatin.R
+
+class StrEditText : AppCompatEditText {
+
+    private lateinit var strIconDrawable: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        strIconDrawable =
+            ContextCompat.getDrawable(context, R.drawable.certificate_image) as Drawable
+        inputType = InputType.TYPE_CLASS_TEXT
+        compoundDrawablePadding = 16
+
+        setHint(R.string.str)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setAutofillHints(AUTOFILL_HINT_USERNAME)
+        }
+        setDrawable(strIconDrawable)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty())
+                    error = context.getString(R.string.et_str_error_message)
+            }
+        })
+    }
+
+    private fun setDrawable(
+        start: Drawable? = null,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
+    }
+}
